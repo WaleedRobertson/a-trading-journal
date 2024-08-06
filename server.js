eb68f99f6f1fd8520d994c54c813a72915fc1375
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-06 13:04:59 -0400

    App is linked to MongoDB via a connection string in the .env file

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 // Here is where we import modules
 // We begin by loading Express
+import dotenv from "dotenv"; 
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
 
 const app = express();
 
