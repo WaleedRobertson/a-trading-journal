97b4766f75ca631655c131ebc86c7b9839e4469f
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-09 10:27:22 -0400

    Adding necessary directories/ files

---
+++ controllers/entries.js
@@ -0,0 +1,8 @@
+// controllers/entries.js
+import express from "express"
+const router = express.Router();
+import User from "./models/user/.js"
+
+// router logic will go here - will be built later on in the lab
+
+module.exports = router;
