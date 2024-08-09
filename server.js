a5c230a810f6a52751ce60ac47e790af029a5422
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-09 09:53:46 -0400

    Controllers directory created

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
+
+
+
+
 
 const app = express();
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
 
-// Route for landing page 
-app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });
+
+
   
